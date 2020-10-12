d4690326ebac1f37b2e800c472f57e20d9356bed
SimonHauser12 <simonhauser32@gmail.com>
2020-10-12 10:07:21 +0200

    SWP

--- 4.Klasse/SWP_ALGO/Hausübungen/Rekursion/src/TestMain.java
+++ 4.Klasse/SWP_ALGO/Hausübungen/Rekursion/src/TestMain.java
@@ -1,38 +1,31 @@
 
 public class TestMain {
-
-	private static int ergebnis=0;
 	
 	public static void main(String[] args) {
 		
-		rekursionsAufruf(15, 11);
+		rekursionsAufruf(50);
 	}
 	
-	public static void rekursionsAufruf(int zahl, int zahl2) {
-		System.out.println(iterativ(zahl, zahl2));
-		ergebnis=0;
-		System.out.print("Rekursiv: "+zahl+" x "+zahl2+" = ");
-		System.out.println(rekursiv(zahl, zahl2));
+	public static void rekursionsAufruf(int zahl) {
+		System.out.println("Iterativ: "+iterativ(zahl));
+		System.out.println("Rekrusiv: "+ rekursiv(zahl));
 		
 	}
 	
-	public static int iterativ(int zahl, int zahl2) {
-		System.out.print("Iterativ: "+zahl+" x "+zahl2+" = ");
-		int counter=0;
-		ergebnis=0;
-		while(counter<zahl2) {
+	public static int iterativ(int zahl) {
+		int ergebnis=0;
+		while(zahl>0) {
 			ergebnis=ergebnis+zahl;
-			counter++;
+			zahl--;
 		}
 		return ergebnis;
 	}
 	
-	public static int rekursiv(int zahl, int zahl2) {
-		if(zahl2>0) {
-			ergebnis=ergebnis+zahl;
-			return rekursiv(zahl, zahl2-1);
+	public static int rekursiv(int zahl) {
+		if(zahl>0) {
+			return zahl+rekursiv(zahl-1);
 		}else {
-			return ergebnis;
+			return 0;
 		}
 	}
 
