d8dd78a83588af3434e84b6984a97b3b204e8626
SimonHauser12 <simonhauser32@gmail.com>
2020-10-14 15:51:35 +0200

    SWP

---
+++ 4.Klasse/SWP_ALGO/Hausübungen/Rekursion_Fibo/src/TestMain.java
@@ -0,0 +1,35 @@
+
+public class TestMain {
+
+	public static void main(String[] args) {
+		
+		funktionsaufruf(10);
+	}
+	
+	public static void funktionsaufruf(int zahl) {
+		System.out.println("Fibonacci Iterativ: "+ interativ(zahl));
+		System.out.println("Fibonacci Rekursiv: "+ fibo_rekursiv(zahl));
+	}
+	
+	public static int interativ(int anzahl) {
+		anzahl-=2;
+		int ergebnis=1;
+		int vor1=1;
+		int vor2=1;
+		while(anzahl>0) {
+			ergebnis=vor1+vor2;
+			vor2=vor1;
+			vor1=ergebnis;
+			anzahl--;
+		}
+		return ergebnis;
+	}
+	
+	public static int fibo_rekursiv(int anzahl) {
+		if (anzahl==1||anzahl==2) {
+			return 1;
+		}else {
+			return fibo_rekursiv(anzahl-1)+fibo_rekursiv(anzahl-2);
+		}
+	  }
+}
