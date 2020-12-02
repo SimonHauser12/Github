6daad82b07addc4de94b40c3f8f9fc3426abe6d5
SimonHauser12 <simonhauser32@gmail.com>
2020-12-02 17:27:20 +0100

    ALGO

--- 4.Klasse/SWP_ALGO/Hausübungen/Aufgabe6_BubbleSort/src/BubbleSort.java
+++ 4.Klasse/SWP_ALGO/Hausübungen/Aufgabe6_BubbleSort/src/BubbleSort.java
@@ -1,9 +1,10 @@
 
 public class BubbleSort {
 
-	static int[] zufallszahlen=new int[20];
+	static int[] zufallszahlen=new int[1000];
 	static long zeit;
 	static long zeit2;
+	static int zaehler=0;
 	
 	public static void main(String[] args) {
 		
@@ -11,25 +12,28 @@ public class BubbleSort {
 		ausgabe("Unsortiert");
 		bubbleSort();
 		ausgabe("Sortiert");
+		System.out.println();
 		System.out.println("Benötigte Zeit: "+ zeit2+" Millisekunden");
+		System.out.println("Durchläufe: "+ zaehler);
 	}
 	
 	public static void befüllen() {
 		for(int i=0; i<zufallszahlen.length; i++) {
-			zufallszahlen[i]=(int)(Math.random()*101);
+			zufallszahlen[i]=(int)(Math.random()*1001);
 		}
 	}
 
 	public static void bubbleSort() {
-		zeit=System.nanoTime();
+		zeit=System.currentTimeMillis();
 		for(int i=zufallszahlen.length; i>1; i--) {
 			for(int j=0; j<i-1; j++) {
 				if(zufallszahlen[j]>zufallszahlen[j+1]) {
+					zaehler++;
 					umtauschen(zufallszahlen[j], zufallszahlen[j+1], j);
 				}
 			}
 		}
-		zeit2=(System.nanoTime()-zeit)/1000;
+		zeit2=System.currentTimeMillis()-zeit;
 	}
 	
 	public static void umtauschen(int a, int b, int index) {
