d5c51102eaf4255923bb635792987b15baaf4df7
SimonHauser12 <simonhauser32@gmail.com>
2022-02-23 18:59:50 +0100

    SWP

--- 5.Klasse/Python/Projekte/Projekt2_ErweiterungHausuebung/TestMain.py
+++ 5.Klasse/Python/Projekte/Projekt2_ErweiterungHausuebung/TestMain.py
@@ -1,4 +1,73 @@
 from random import randint
+import time
+
+class ArrayListe:
+
+    def __init__(self):
+        self.list=[]
+
+    def insert(self, obj):
+        self.list.append(obj)
+
+    def insertAfterPlace(self, pl, newObj):
+        if(pl+1<len(self.list) and pl>=0):
+            self.list.insert(pl+1, newObj)
+        elif():
+            print("Nur zwischen 0 und "+str(len(self.list)-2)+" wählen")
+        
+    def insertBeforePlace(self, pl, newObj):
+        if(pl<len(self.list) and pl-1>=0):
+            self.list.insert(pl-1, newObj)
+        elif():
+            print("Nur zwischen 1 und "+str(len(self.list)-1)+" wählen")
+          
+    def deleteAfterPlace(self, pl):
+        if(pl+1<len(self.list) and pl>=0):
+            self.list.pop(pl+1)
+        elif():
+            print("Nur zwischen 0 und "+str(len(self.list)-2)+" wählen")
+
+    def deleteBeforePlace(self, pl):
+        if(pl<len(self.list) and pl-1>=0):
+            self.list.pop(pl-1)
+        elif():
+            print("Nur zwischen 1 und "+str(len(self.list)-1)+" wählen")
+
+    def findObj(self, pl):
+        print("An der Stelle "+str(pl)+"befindet sich das Element "+ str(self.list[pl]))
+
+    def select(self):
+        for i in self.list:
+            print("|"+str(i)+"|")
+    
+    def selectReverse(self):
+        for i in reversed(self.list):
+            print("|"+str(i)+"|")
+
+    def length(self):
+        print ("Länge Arraylist: "+str(len(self.list)))
+
+    def sortAsc(self):
+        j = -1
+        key = self.list[0]
+        for i in range(1, len(self.list)):
+            key = self.list[i]
+            j = i-1
+            while(j >= 0 and self.list[j] > key):
+                self.list[j+1] = self.list[j]
+                j -= 1
+            self.list[j+1] = key
+
+    def sortDesc(self):
+        j = -1
+        key = self.list[0]
+        for i in range(1, len(self.list)):
+            key = self.list[i]
+            j = i-1
+            while(j >= 0 and self.list[j] < key):
+                self.list[j+1] = self.list[j]
+                j -= 1
+            self.list[j+1] = key
 
 class ElementListe:
     counter=0
@@ -59,7 +128,6 @@ class VerketteListe:
         le=self.startEl
         for i in range(pl):
             le=le.getnextElement()
-
         return le
 
     def insert(self, obj):
@@ -71,7 +139,7 @@ class VerketteListe:
             newEl.prevEl=lastEl
             lastEl.nextEl=newEl  
 
-    def insertAfter(self, obj, newObj):
+    def insertAfterObj(self, obj, newObj):
         le=self.getElement(obj)
         if(le.__str__()==obj):
             if(le.getnextElement()!=None):
@@ -91,9 +159,9 @@ class VerketteListe:
                 newEl.nextEl=nle          
         elif(le.__str__()!=obj):
             print("Element "+str(obj)+" ist nicht in der Liste enthalten")
-            print(str(newObj)+" konnte nicht hinzugefügt werden")  
+            print(str(newObj)+" konnte nicht hinzugefügt werden") 
 
-    def insertBefore(self, obj, newObj):
+    def insertBeforeObj(self, obj, newObj):
         le=self.getElement(obj)
         if(le.__str__()==obj):
             if(le.getprevElement()!=None):
@@ -114,9 +182,9 @@ class VerketteListe:
                 self.startEl=newEl          
         elif(le.__str__()!=obj):
             print("Element "+str(obj)+" ist nicht in der Liste enthalten")
-            print(str(newObj)+" konnte nicht hinzugefügt werden") 
+            print(str(newObj)+" konnte nicht hinzugefügt werden")  
 
-    def defeteAfter(self, obj):
+    def deleteAfterObj(self, obj):
         le=self.getElement(obj)
         if(le.__str__()==obj):
             if(le.getnextElement()!=None):
@@ -138,7 +206,7 @@ class VerketteListe:
             print("Element "+str(obj)+" ist nicht in der Liste enthalten")
             print("Kein Delete konnte durchgeführt werden")     
         
-    def deleteBefore(self, obj):
+    def deleteBeforeObj(self, obj):
         le=self.getElement(obj)
         if(le.__str__()==obj):
             if(le.getprevElement()!=None):
@@ -178,7 +246,96 @@ class VerketteListe:
                 print("Vorheriges Element: "+str(le.getprevElement().__str__()))
                 print("Nachfolgendes Element nicht vorhanden (letztes Element)")      
         elif(le.__str__()!=obj):
-            print("Element "+str(obj)+" ist nicht in der Liste enthalten")   
+            print("Element "+str(obj)+" ist nicht in der Liste enthalten")  
+
+    def insertAfterPlace(self, pl, newObj):
+        if(pl>=ElementListe.counter or pl<0):
+            print("Es wurde eine falsche Zahl gewählt")
+            print("Bitte zwischen 0-"+str(ElementListe.counter-1)+" wählen")
+        elif(pl<ElementListe.counter):
+            le=self.findElement(pl)
+            if(le.getnextElement()!=None):
+                nle=le.getnextElement()
+
+                newEl=ElementListe(newObj)
+                newEl.prevEl=le
+                le.nextEl=newEl
+                newEl.nextEl=nle
+                nle.prevEl=newEl  
+            elif(le.getnextElement()==None):
+                nle=None
+
+                newEl=ElementListe(newObj)
+                newEl.prevEl=le
+                le.nextEl=newEl
+                newEl.nextEl=nle 
+
+    def insertBeforePlace(self, pl, newObj):
+        if(pl>=ElementListe.counter or pl<0):
+            print("Es wurde eine falsche Zahl gewählt")
+            print("Bitte zwischen 0-"+str(ElementListe.counter-1)+" wählen")
+        elif(pl<ElementListe.counter):
+            le=self.findElement(pl)
+            if(le.getprevElement()!=None):
+                ple=le.getprevElement()
+
+                newEl=ElementListe(newObj)
+                le.prevEl=newEl
+                newEl.nextEl=le
+                ple.nextEl=newEl
+                newEl.prevEl=ple  
+            elif(le.getprevElement()==None):
+                ple=None
+
+                newEl=ElementListe(newObj)
+                le.prevEl=newEl
+                newEl.nextEl=le
+                newEl.prevEl=ple  
+                self.startEl=newEl 
+
+    def deleteAfterPlace(self, pl):
+        if(pl>=ElementListe.counter or pl<0):
+            print("Es wurde eine falsche Zahl gewählt")
+            print("Bitte zwischen 0-"+str(ElementListe.counter-1)+" wählen")
+        elif(pl<ElementListe.counter):
+            le=self.findElement(pl)
+            if(le.getnextElement()!=None):
+                dle=le.getnextElement()
+                if(dle.getnextElement()!=None):
+                    ndle=dle.getnextElement()
+
+                    le.nextEl=ndle
+                    ndle.prevEl=le
+                elif(dle.getnextElement()==None):
+                    ndle=None
+
+                    le.nextEl=ndle
+
+                ElementListe.counter-=1
+            elif(le.getnextElement()==None):
+                print("Es ist kein Element nach der Stelle "+str(pl)+" vorhanden, kein Delete durchführbar")   
+        
+    def deleteBeforePlace(self, pl):
+        if(pl>=ElementListe.counter or pl<0):
+            print("Es wurde eine falsche Zahl gewählt")
+            print("Bitte zwischen 0-"+str(ElementListe.counter-1)+" wählen")
+        elif(pl<ElementListe.counter):
+            le=self.findElement(pl)
+            if(le.getprevElement()!=None):
+                dle=le.getprevElement()
+                if(dle.getprevElement()!=None):
+                    pdle=dle.getprevElement()
+
+                    le.prevEl=pdle
+                    pdle.nextEl=le
+                elif(dle.getprevElement()==None):
+                    pdle=None
+
+                    le.prevEl=pdle
+
+                ElementListe.counter-=1
+            elif(le.getprevElement()==None):
+                print("Es ist kein Element vor der Stelle "+str(pl)+" vorhanden, kein Delete durchführbar")     
 
     def findObj(self, pl):
         if(pl>=ElementListe.counter or pl<0):
@@ -197,7 +354,7 @@ class VerketteListe:
             elif(le.getnextElement()==None):
                 print("An der Stelle "+str(pl)+". befindet sich das Element "+str(le.__str__()))
                 print("Vorheriges Element: "+str(le.getprevElement().__str__()))
-                print("Nachfolgendes Element nicht vorhanden (letztes Element)") 
+                print("Nachfolgendes Element nicht vorhanden (letztes Element)")
 
     def select(self):
         print("Normal")
@@ -216,33 +373,93 @@ class VerketteListe:
             print("| " + str(rle.__str__()) + " |")
             rle=rle.getprevElement()
 
-     
+    def sortAsc(self):
+        j = -1
+        key = self.findElement(0)
+        for i in range(1, int(ElementListe.counterListe())):
+            key = self.findElement(i).__str__()
+            j = i-1
+            while(j >= 0 and self.findElement(j).__str__() > key):
+                l=self.findElement(j).__str__()
+                self.deleteAfterPlace(j)
+                self.insertAfterPlace(j, l)
+                j -= 1
+            if(j>=0):
+                self.deleteAfterPlace(j)
+                self.insertAfterPlace(j, key)
+            elif(j<0):
+                self.deleteBeforePlace(1)
+                self.insertBeforePlace(1, key)
+
+    def sortDesc(self):
+        j = -1
+        key = self.findElement(0)
+        for i in range(1, int(ElementListe.counterListe())):
+            key = self.findElement(i).__str__()
+            j = i-1
+            while(j >= 0 and self.findElement(j).__str__() < key):
+                l=self.findElement(j).__str__()
+                self.deleteAfterPlace(j)
+                self.insertAfterPlace(j, l)
+                j -= 1
+            if(j>=0):
+                self.deleteAfterPlace(j)
+                self.insertAfterPlace(j, key)
+            elif(j<0):
+                self.deleteBeforePlace(1)
+                self.insertBeforePlace(1, key)
+                
+             
+            
 
 def Main():
+    """startzeit=time.time()
     liste=VerketteListe()
-    liste.insert(52)
-    for i in range(12):
-        zufallszahl=randint(10,99)
+    for i in range(1000):
+        zufallszahl=randint(1,9)
         liste.insert(zufallszahl)
-    liste.insert(85)
-    liste.select()
+    #liste.select()
+    endzeit=time.time()
     print("Anzahl der List-Elemente: "+ ElementListe.counterListe())
-    #print("-------------------------------------------------")
+    print("Dauer: "+ str(endzeit-startzeit))
 
     #Fertig:
-    #liste.selectReverse()
-    #liste.insertBefore(52, 10)
-    #liste.insertAfter(85, 12)
-    #liste.defeteAfter(85)
-    #liste.deleteBefore(52)
-    #liste.findObjPlace(85)
-    #liste.findObj(13)
-    
-    liste.findObj(13)
     #liste.select()
+    #liste.selectReverse()
     #print("Anzahl der List-Elemente: "+ ElementListe.counterListe())
+
+    #Anhand der Objekte:
+    #liste.insertBeforeObj(52, 10)
+    #liste.insertAfterObj(85, 12)
+    #liste.deleteAfterObj(85)
+    #liste.deleteBeforeObj(52)
+    #liste.findObjPlace(85)
+    #-----------------------------
+    #Anhand der Stellen
+    #liste.insertBeforePlace(5, 10)
+    #liste.insertAfterPlace(5, l)
+    #liste.deleteAfterPlace(5)
+    #liste.deleteBeforePlace(12)
+    #liste.findObj(13)
+    #liste.sortDesc()
+    #liste.sortAsc()
+    start=time.time()
+    liste.sortAsc()
+    ende=time.time()
+    print("Dauer Sortierung: "+ str(ende-start))
+    #liste.select()
+    
     #liste.delete(12)
-    #print("Anzahl der List-Elemente: "+ ElementListe.counterListe())
+    #print("Anzahl der List-Elemente: "+ ElementListe.counterListe())"""
+
+    aliste=ArrayListe()
+    for i in range(10):
+        zufallszahl=randint(1,9)
+        aliste.insert(zufallszahl)
+    aliste.select()
+    aliste.sortDesc()
+    print("-----------------")
+    aliste.select()
         
 
 if __name__ == "__main__":
